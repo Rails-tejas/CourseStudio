08080a70d8f167d06e971c367d0447a2c110c8a9
railstejas <tejascryptex@gmail.com>
2025-11-17 18:17:05 +0530

    Remove test directory from Git tracking

--- test/controllers/certificates_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CertificatesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get certificates_show_url
-    assert_response :success
-  end
-end
--- test/controllers/video_progress_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class VideoProgressControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get video_progress_create_url
-    assert_response :success
-  end
-end
